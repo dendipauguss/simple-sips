0e9d4d6b3d6031454ddcb1d312d6632dd4b0d9e7
Dendi Paugus Sukmaya <dendipauguss1111@gmail.com>
2025-12-03 16:55:22 +0700

    dev - add chart to dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,15 +7,26 @@
                     <div class="card">
                         <div class="card-body">
                             <h5 class="card-title">Status Pengenaan</h5>
-                            <!-- Pie Chart -->
+                            <!-- Status Chart -->
                             <canvas id="_dm-pieChart" width="464" height="463"
                                 style="display: block; box-sizing: border-box; height: 370.4px; width: 371.2px;"
                                 data-status='@json($status_data)'></canvas>
-                            <!-- END : Pie Chart -->
+                            <!-- END : Status Chart -->
                         </div>
                     </div>
                 </div>
                 <div class="col-md-4 mb-3">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Periode</h5>
+                            <!-- Pie Chart -->
+                            <canvas id="_dm-barChart" width="464" height="463"
+                                style="display: block; box-sizing: border-box; height: 370.4px; width: 371.2px;"></canvas>
+                            <!-- END : Pie Chart -->
+                        </div>
+                    </div>
+                </div>
+                {{-- <div class="col-md-4 mb-3">
                     <div class="card">
                         <div class="card-body">
                             <h5 class="card-title">Status </h5>
@@ -25,18 +36,7 @@
                             <!-- END : Pie Chart -->
                         </div>
                     </div>
-                </div>
-                <div class="col-md-4 mb-3">
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title">Pie chart</h5>
-                            <!-- Pie Chart -->
-                            <canvas id="_dm-barChart" width="464" height="463"
-                                style="display: block; box-sizing: border-box; height: 370.4px; width: 371.2px;"></canvas>
-                            <!-- END : Pie Chart -->
-                        </div>
-                    </div>
-                </div>
+                </div> --}}
             </div>
         </div>
     </div>
